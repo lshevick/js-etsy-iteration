e9d8e9b0aa3c9fb7fdd9b6807e315dd0548510aa
Levi Shevick <levidshevick@gmail.com>
2022-05-10 14:21:28 -0400

    adds refactored averageCost function w/ lodash

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const averageCost = arr => {
     // get prices from objects inside items array
     const prices = arr.map(e => e.price);
     // average prices
-    let result = prices.reduce((a, b) => a + b) / prices.length;
+    let result = _.mean(prices);
     //formats price to standard USD format
     result = '$' + result.toPrecision(4);
     console.log(`The average price is ${result}`);
